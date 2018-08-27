d1e403e1f218f0041027a497c756c97b6348dca5
Rajat Khanna <legalimpurity@gmail.com>
2018-08-27 11:18:36 +0530

    Dynamic Table View Code

---
+++ DynamicTableView/StudentClassCell.swift
@@ -0,0 +1,16 @@
+//
+//  StudentClassCell.swift
+//  DynamicTableView
+//
+//  Created by Rajat Khanna on 21/08/18.
+//  Copyright © 2018 Amity University. All rights reserved.
+//
+
+import UIKit
+
+class StudentClassCell: UITableViewCell {
+    
+    @IBOutlet weak var StudentNameLabel: UILabel!
+    @IBOutlet weak var StudentClass: UILabel!
+    
+}
--- DynamicTableView/ViewController.swift
+++ DynamicTableView/ViewController.swift
@@ -8,10 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var MyTableView: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return StudentNameDataSource.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let tableelement = tableView.dequeueReusableCell(withIdentifier: "NameClass") as! StudentClassCell
+        tableelement.StudentNameLabel.text = StudentNameDataSource[indexPath.row]
+        tableelement.StudentClass.text = StudentClassDataSource[indexPath.row]
+        return tableelement
+    }
+    
+    var StudentNameDataSource = Array<String>()
+    var StudentClassDataSource = Array<String>()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        StudentNameDataSource.append("Vivek")
+        StudentClassDataSource.append("5CSE-4")
+
+        StudentNameDataSource.append("Sandru")
+        StudentClassDataSource.append("3CSE-4")
+
+        StudentNameDataSource.append("Sarthak")
+        StudentClassDataSource.append("1CSE-3")
+
+        StudentNameDataSource.append("Abhinav")
+        StudentClassDataSource.append("1CSE-3")
+
+        StudentNameDataSource.append("Animesh")
+        StudentClassDataSource.append("7CSE-5")
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +52,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func addClicker(_ sender: Any) {
+        StudentNameDataSource.append("Animesh")
+        StudentClassDataSource.append("7CSE-5")
+        MyTableView.reloadData()
+        }
+    
+    
+    
 }
 
